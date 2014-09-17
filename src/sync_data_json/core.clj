67bee94cf7b9b54108bf8e90f12f0644e3fafc2a
Sean Brewer <seabre986@gmail.com>
2014-09-17 17:07:31 -0400

    Work on loading of entries from program start.

--- src/sync_data_json/core.clj
+++ src/sync_data_json/core.clj
@@ -7,10 +7,19 @@
 (defn read-data-json [url]
   (parse-stream (clojure.java.io/reader url) true))
 
-(defn load-entries [url]
+(defn load-entries [url host]
   (doseq [x (read-data-json url)]
-    (entries/load-entry x)))
+    (entries/load-entry x host)))
 
 (defn -main [& m]
-  (load-entries (env :data-json-url))
-  (println "Entries loaded."))
+  (load-entries (env :sync-data-json-url) (env :sync-host))
+  (println "Entries loaded.")
+  (doseq [x (entries/get-new-entries)]
+    (let [jsonentry (read-string (get x :serialized_data))]
+      (entries/create-external-dataset-from-entry jsonentry (env :sync-host) (env :sync-url) (env :sync-username) (env :sync-password) (env :sync-token))))
+  (println "New datasets created.")
+  (doseq [x (entries/get-updated-entries)]
+    (let [jsonentry (read-string (get x :serialized_data))
+          destination-id (get x :destination_id)]
+      (entries/update-external-dataset-from-entry destination-id jsonentry (env :sync-host) (env :sync-url) (env :sync-username) (env :sync-password) (env :sync-token))))
+  (println "Entries updated."))
